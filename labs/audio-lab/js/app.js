14e5ba1f0df5c331572823ba50e14aba574c5318
Billy <mcintyrebilly3@gmail.com>
2019-12-10 19:16:59 -0500

    added button names

--- labs/audio-lab/js/app.js
+++ labs/audio-lab/js/app.js
@@ -1,13 +1,19 @@
 var soundButtons = document.getElementById("soundButtons");
 
-var sounds = ["chimes_long", "click_clock_loop", "pop_10", "puff", "rustle_5"];
+var sounds = [
+  { file: "chimes_long.mp3", name: "Chimes" },
+  { file: "click_clock_loop.mp3", name: "Click Clock" },
+  { file: "pop_10.mp3", name: "Pop" },
+  { file: "puff.mp3", name: "Puff" },
+  { file: "rustle_5.mp3", name: "Rustle" }
+];
 
 var soundElements = [];
 
 // loop through all of the sounds and create audio tags for them
 sounds.forEach((soundURL, idx) => {
   // the sound
-  var newSound = new Audio("sounds/" + soundURL + ".mp3");
+  var newSound = new Audio("sounds/" + soundURL.file);
   //   var soundNames = ["Chimes", "Click Clock", "Pop", "Puff", "Rustle"];
 
   //   store each sound in an array for later reference
@@ -16,7 +22,7 @@ sounds.forEach((soundURL, idx) => {
   //   create the button to play the sound
   var newButton = document.createElement("button");
   //   change the text that goes in the buttons from new array
-  newButton.innerHTML = soundURL;
+  newButton.innerHTML = soundURL.name;
 
   //   store the sounds index
   newButton.setAttribute("data-sound-id", idx);
